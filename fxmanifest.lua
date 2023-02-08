909300e862f1b9e885c052e1a9a0646882d94037
Kutay.Ozberak.Contractor <kutay.ozberak.contractor@pepsico.com>
2023-02-08 10:48:31 +0300

    change manifest file type

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,4 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
 
 description 'Yurima Notify'
 
@@ -17,3 +17,5 @@ files {
 }
 
 export 'pushNotification'
+
+lua54 'yes'
